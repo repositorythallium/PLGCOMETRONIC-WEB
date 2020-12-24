9d77a348fcad7644c00885f1ab5af2b34a021ffe
Repository (Desenvolvimento) <repository.desenvolvimento@gmail.com>
2020-12-23 22:35:46 -0300

    PLGECOMMERCEWEB-19122020212325 \n# Sobrescrevendo versão dos projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './component/cart/cart.component';
+import { CheckoutComponent } from './component/checkout/checkout.component';
+import { ConfirmacaocompraComponent } from './component/confirmacaocompra/confirmacaocompra.component';
+import { HomeComponent } from './component/home/home.component';
+import { ProdutoComponent } from './component/produto/produto.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", 
+    component: HomeComponent
+  },
+  {
+    path: "produto/:codigo", 
+    component: ProdutoComponent
+  },
+  {
+    path: "cart", 
+    component: CartComponent
+  },
+  {
+    path: "checkout", 
+    component: CheckoutComponent
+  },
+  {
+    path: "confirmacao", 
+    component: ConfirmacaocompraComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './component/home/home.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { CartComponent } from './component/cart/cart.component';
+import { CheckoutComponent } from './component/checkout/checkout.component';
+import { ProdutoComponent } from './component/produto/produto.component';
+import { ConfirmacaocompraComponent } from './component/confirmacaocompra/confirmacaocompra.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    CartComponent,
+    CheckoutComponent,
+    ProdutoComponent,
+    ConfirmacaocompraComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
